a00f460e657daa99d6f23a137c25c33d5ad77db0
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2023-10-20 19:17:54 -0500

    huawei[X] k8s manifiesto jdk 17

--- src/main/java/hackathon/nttdata/coderpath/respuestas/RespuestasApplication.java
+++ src/main/java/hackathon/nttdata/coderpath/respuestas/RespuestasApplication.java
@@ -2,9 +2,9 @@ package hackathon.nttdata.coderpath.respuestas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RespuestasApplication {
 
